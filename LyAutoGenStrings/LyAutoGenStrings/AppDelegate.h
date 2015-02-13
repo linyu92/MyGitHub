3621021a013912eb668453cd0d825701018d2e04
linyu92 <levi92linyu@gmail.com>
2015-02-13 16:21:18 +0800

    new

---
+++ LyAutoGenStrings/LyAutoGenStrings/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LyAutoGenStrings
+//
+//  Created by linyu on 2/13/15.
+//  Copyright (c) 2015 linyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LyAutoGenStrings/LyAutoGenStrings/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LyAutoGenStrings
+//
+//  Created by linyu on 2/13/15.
+//  Copyright (c) 2015 linyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
